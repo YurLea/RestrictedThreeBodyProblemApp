5bda1cc96c2f24179b125b5e0e861427bf96c9a8
YurLea <vsailevyrui@gmail.com>
2025-10-07 16:43:03 +0300

    general preparing before app 1

--- Scripts/TheApp.py
+++ Scripts/TheApp.py
@@ -1,35 +1,13 @@
 import numpy as np
-from scipy.integrate import solve_ivp
+from MethodsForSolving import solve_system
 import matplotlib.pyplot as plt
 
-
-def nonlinear_system(t, states):
-    """
-    Restricted 3 body problem
-    """
-    x, y, u, v = states
-
-    mu = 0.5
-
-    r1 = np.sqrt((x - mu)**2 + y**2)
-    r2 = np.sqrt((x - mu + 1)**2 + y**2)
-
-    dxdt = u
-    dydt = v
-    dudt = (2*v + (1 - mu) * (x - mu) + mu * (x + 1 - mu) -
-            (1-mu) * (x - mu) / r1**3 - mu * (x - mu + 1) / r2**3)
-    dvdt = (-2*u + (1 - mu) * y + mu * y -
-            (1-mu) * y / r1**3 - mu * y / r2**3)
-
-    return [dxdt, dydt, dudt, dvdt]
-
 # Solultion
-init_states = [0.6, 0.0, 0, 1.0]
-t_span = (0, 5)
-t_eval = np.linspace(0, 5, 100000)
+init_states = [0.65, 0.0, 0, 2.07]
+t_span = (0, 30)
+t_eval = np.linspace(0, 30, 200000)
 
-sol = solve_ivp(nonlinear_system, t_span, init_states, t_eval=t_eval,
-                method='RK45', rtol=1e-12, atol=1e-12)
+sol = solve_system(init_states, 'RK45', t_span, t_eval)
 
 # Visualization
 plt.plot(sol.y[0], sol.y[1])
