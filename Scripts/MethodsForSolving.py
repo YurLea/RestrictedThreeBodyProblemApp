dcef16acf89f356bbe12832970a80e4c05a70d85
YurLea <vsailevyrui@gmail.com>
2025-10-08 19:37:01 +0300

    general preparing before app 2

--- Scripts/MethodsForSolving.py
+++ Scripts/MethodsForSolving.py
@@ -1,26 +1,25 @@
 from scipy.integrate import solve_ivp
 import numpy as np
 
-def system_of_problem(t, states):
+
+def system_of_problem(t, states, mu): # mass parameter
     """
     Restricted 3 body problem
     """
     x, y, u, v = states
 
-    mu = 0.5
-
-    r1 = np.sqrt((x - mu)**2 + y**2)
-    r2 = np.sqrt((x - mu + 1)**2 + y**2)
+    r1 = np.sqrt((x - mu) ** 2 + y ** 2)
+    r2 = np.sqrt((x - mu + 1) ** 2 + y ** 2)
 
     dxdt = u
     dydt = v
-    dudt = (2*v + (1 - mu) * (x - mu) + mu * (x + 1 - mu) -
-            (1-mu) * (x - mu) / r1**3 - mu * (x - mu + 1) / r2**3)
-    dvdt = (-2*u + (1 - mu) * y + mu * y -
-            (1-mu) * y / r1**3 - mu * y / r2**3)
+    dudt = (2 * v + (1 - mu) * (x - mu) + mu * (x + 1 - mu) -
+            (1 - mu) * (x - mu) / r1 ** 3 - mu * (x - mu + 1) / r2 ** 3)
+    dvdt = (-2 * u + (1 - mu) * y + mu * y -
+            (1 - mu) * y / r1 ** 3 - mu * y / r2 ** 3)
 
     return [dxdt, dydt, dudt, dvdt]
 
-def solve_system(states, method, t_span, t_eval):
+def solve_system(states, method, t_span, t_eval, mu):
     return solve_ivp(system_of_problem, t_span, states, t_eval=t_eval,
-                method=method, rtol=1e-12, atol=1e-12)
+                method=method, rtol=1e-12, atol=1e-12, args=(mu,))
--- Scripts/TheApp.py
+++ Scripts/TheApp.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 
 # Solultion
 init_states = [0.65, 0.0, 0, 2.07]
+method = 'RK45'
 t_span = (0, 30)
 t_eval = np.linspace(0, 30, 200000)
 
-sol = solve_system(init_states, 'RK45', t_span, t_eval)
+sol = solve_system(init_states, method, t_span, t_eval, 0.5)
 
 # Visualization
 plt.plot(sol.y[0], sol.y[1])
